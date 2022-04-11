030d22200178bd33a9389873669f64828d61ed82
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2022-04-11 18:23:38 +0300

    Add font family and implement header component

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header className="flex rounded-xl shadow-md justify-between p-3 bg-white">
+      <h1 className="italic font-semibold">UPayments Store</h1>
+      <button className="italic font-semibold">Register</button>
+    </header>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { Header } from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-[#ECECEC] p-5 h-screen w-screen">
       <Head>
         <title>UPayments Store</title>
         <meta name="description" content="UPayments Home Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
     </div>
   );
 };
